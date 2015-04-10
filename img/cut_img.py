a53ebbc77111a6eb5467d4c79970d3cc203d1fcf
Xianda Yu <176912231@163.com>
2015-04-10 11:03:55 +0800

    Update cut_img.py

--- img/cut_img.py
+++ img/cut_img.py
@@ -13,32 +13,35 @@ import os
 """                                                                                                       
                                                                                                           
 
-def trans_xy(x, y, v_x, v_y, point_a, point_b):                                                           
-    ''' conversion of coordinate                                                                          
-        @x/y length/width of pic
-        @v_x/v_y length/width of virtual pic                                                              
-        @point_a a(x, y)                                                                                  
-        @point_b b(x, y)
-        @return regoin(x0, y0, x1, y1)                                                                    
-    ''' 
-    a_x = point_a[0]*x/v_x                                                                                
-    a_y = point_a[1]*y/v_y                                                                                
-    b_x = point_b[0]*x/v_x                                                                                
-    b_y = point_b[1]*y/v_y 
-    return (a_x, a_y, b_x, b_y)                                                                           
-                                                                                                          
-
-def cut_img(pic, spic, region):                                                                           
+def cut_img(pic, v_x, v_y, point_a, point_b):
     ''' cut img
         @pic source pic
-        @spic save pic path
-        @region (x0, y0, x1, y1)                                                                          
+        @v_x/y virtual x and y
+        @port_a/b point(x, y)
     '''
     try:
-        img = Image.pen(pic)
-        crop_img = img.crop(region)
-        crop_img.save(spic + '.jpg')
-        shutil.move(spic + '.jpg', spic)                                                                  
-    except:
-        return False                                                                                      
+        img = Image.open(pic)
+        (x, y) = img.size
+
+        def trans_xy(x, y, v_x, v_y, point_a, point_b):
+            ''' conversion of coordinate
+                @x/y length/width of pic
+                @v_x/v_y length/width of virtual pic
+                @point_a a(x, y)
+                @point_b b(x, y)
+                @return regoin(x0, y0, x1, y1)
+            '''
+            a_x = point_a[0]*x/v_x
+            a_y = point_a[1]*y/v_y
+            b_x = point_b[0]*x/v_x
+            b_y = point_b[1]*y/v_y
+            return (a_x, a_y, b_x, b_y)
+
+        crop_img = img.crop(trans_xy(x, y, v_x, v_y, point_a, point_b))
+        crop_img.save(pic + '.jpg')
+        os.remove(pic)
+        shutil.move(pic + '.jpg', pic)
+    except Exception as e:
+        print e
+        return False
     return True
