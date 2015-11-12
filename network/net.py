a9df77c7faf7bd9abb823421db6e804cd90af781
Xianda Yu <176912231@163.com>
2015-11-12 15:26:01 +0800

    Update net.py

--- network/net.py
+++ network/net.py
@@ -6,6 +6,7 @@
 
 import os
 import re
+import socket
 
 
 def Int2StringNetmask(netmaskInt):
@@ -53,6 +54,67 @@ def gen_ip_address(ip, netmask):
     127.0.0.1 255.255.255.0 生成 127.0.0.1/24
     """
     return '%s/%d' % (ip, String2IntNetmask(netmask))
+
+
+def isIPV4(ipv4):
+    try:
+        socket.inet_aton(ipv4)
+    except socket.error:
+        return False
+    return True
+
+
+def isNETMASK(netmask):
+    NETMASK = (
+        "0.0.0.0",
+        "128.0.0.0", 
+        "192.0.0.0",
+        "224.0.0.0",
+        "240.0.0.0",
+        "248.0.0.0",
+        "252.0.0.0",
+        "254.0.0.0",
+        "255.0.0.0",
+        "255.128.0.0", 
+        "255.192.0.0",
+        "255.224.0.0",
+        "255.240.0.0",
+        "255.248.0.0",
+        "255.252.0.0",
+        "255.254.0.0",
+        "255.255.0.0",
+        "255.255.128.0",
+        "255.255.192.0", 
+        "255.255.224.0",
+        "255.255.240.0",
+        "255.255.248.0",
+        "255.255.252.0",
+        "255.255.254.0",
+        "255.255.255.0",
+        "255.255.255.128",  
+        "255.255.255.192",
+        "255.255.255.224",
+        "255.255.255.240",
+        "255.255.255.248",
+        "255.255.255.252",
+        "255.255.255.254",
+        "255.255.255.255",
+    )
+    if netmask not in NETMASK:
+        return False
+    return True
+
+
+def isIPV6(ipv6):
+    ipv6 = ipv6.split('/')
+    if len(ipv6) != 2:
+        return False
+    try:
+        socket.inet_pton(socket.AF_INET6, ipv6[0])
+    except socket.error:
+        return False
+    return True
+
 
 
 def shut_down(cancel=False, delay=10):
